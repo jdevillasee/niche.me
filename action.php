b4166c91c7c0007665f3b73c8d0fe2a1e2e99dff
Janay <jdevill@emory.edu>
2015-10-18 03:54:30 -0400

    -m "contact page"

--- action.php
+++ action.php
@@ -1,1 +1,4 @@
 <?php echo htmlspecialchars($_POST['inputemail']); ?>
+<?php echo htmlspecialchars($_POST['name']); ?>
+<?php echo htmlspecialchars($_POST['email']); ?>
+<?php echo htmlspecialchars($_POST['message']); ?>
